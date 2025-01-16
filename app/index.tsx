7b93c2a3e2361c97d2b72a3406beca1ef8aff602
alvarado0925 <alvarado0925@gmail.com>
2025-01-15 23:55:49 -0500

    Agregar componente Button y mejorar la presentación de la calculadora

--- app/index.tsx
+++ app/index.tsx
@@ -3,12 +3,85 @@ import { View } from 'react-native';
 
 import ThemeText from '@/components/ThemeText';
 import { globalStyles } from '@/styles/global-styles';
+import Button from '@/components/Button';
+import { Colors } from '@/constants/Colors';
 
 const CalculatorApp = () => {
   return (
     <View style={globalStyles.calculatorContainer}>
-      <ThemeText>50 x 50</ThemeText>
-      <ThemeText variant="h2">25000</ThemeText>
+      <View style={{ paddingHorizontal: 30, paddingBottom: 20 }}>
+        <ThemeText>50 x 50</ThemeText>
+        <ThemeText variant="h2">25000</ThemeText>
+      </View>
+
+      <View style={globalStyles.calculatorRow}>
+        <Button
+          label="C"
+          color={Colors.lightGray}
+          blackText
+          onPress={() => console.log(1)}
+        />
+        <Button
+          label="+/-"
+          color={Colors.lightGray}
+          blackText
+          onPress={() => console.log(1)}
+        />
+        <Button
+          label="del"
+          color={Colors.lightGray}
+          blackText
+          onPress={() => console.log(1)}
+        />
+        <Button
+          label="÷"
+          color={Colors.orange}
+          onPress={() => console.log(1)}
+        />
+      </View>
+
+      <View style={globalStyles.calculatorRow}>
+        <Button label="7" onPress={() => console.log(1)} />
+        <Button label="8" onPress={() => console.log(1)} />
+        <Button label="9" onPress={() => console.log(1)} />
+        <Button
+          label="x"
+          color={Colors.orange}
+          onPress={() => console.log(1)}
+        />
+      </View>
+
+      <View style={globalStyles.calculatorRow}>
+        <Button label="4" onPress={() => console.log(1)} />
+        <Button label="5" onPress={() => console.log(1)} />
+        <Button label="6" onPress={() => console.log(1)} />
+        <Button
+          label="-"
+          color={Colors.orange}
+          onPress={() => console.log(1)}
+        />
+      </View>
+
+      <View style={globalStyles.calculatorRow}>
+        <Button label="1" onPress={() => console.log(1)} />
+        <Button label="2" onPress={() => console.log(1)} />
+        <Button label="3" onPress={() => console.log(1)} />
+        <Button
+          label="+"
+          color={Colors.orange}
+          onPress={() => console.log(1)}
+        />
+      </View>
+
+      <View style={globalStyles.calculatorRow}>
+        <Button label="0" doubleSize onPress={() => console.log(1)} />
+        <Button label="." onPress={() => console.log(1)} />
+        <Button
+          label="="
+          color={Colors.orange}
+          onPress={() => console.log(1)}
+        />
+      </View>
     </View>
   );
 };
--- styles/global-styles.ts
+++ styles/global-styles.ts
@@ -23,4 +23,27 @@ export const globalStyles = StyleSheet.create({
     textAlign: 'right',
     fontWeight: '300',
   },
+  calculatorRow: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+    marginBottom: 18,
+  },
+  button: {
+    width: 80,
+    height: 80,
+    borderRadius: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 10,
+    backgroundColor: Colors.darkGray,
+  },
+  buttonText: {
+    color: Colors.textPrimary,
+    fontSize: 30,
+    fontWeight: '300',
+    textAlign: 'center',
+    padding: 10,
+    fontFamily: 'SpaceMono',
+  },
 });
